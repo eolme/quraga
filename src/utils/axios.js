28d12c5f0b8332373f267c947893e576fdb3541b
Anton Petrov <im@petrov.engineer>
2020-05-03 19:35:57 +0400

    refactor: back to axios because redaxios is buggy

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,58 +1,6 @@
-import 'unfetch/polyfill';
-import redaxios from 'redaxios';
+import axios from 'axios';
+import retry from 'axios-retry';
 
-const createURL = (path, base) => {
-  path = path ? String(path) : '/';
-  return base ? base + path : path;
-};
+retry(axios, { retries: 3, shouldResetTimeout: true });
 
-const enchanceMethod = (method, defaults) => {
-  return (url, data, config) => {
-    if (!config) {
-      data = data || {};
-      config = data;
-    }
-
-    const parsedURL = createURL(url, config.baseURL || defaults.baseURL);
-
-    return new Promise((resolve, reject) => {
-      const parse = (result) => {
-        try {
-          result.data = JSON.parse(result.data);
-        } catch {
-          result.data = Object(result.data);
-        }
-        resolve(result);
-      };
-
-      const request = () => {
-        return method(parsedURL, data, config).then(parse);
-      };
-
-      let promise = request();
-      let count = 2;
-
-      while (count > 0) {
-        promise = promise.catch(request);
-        --count;
-      }
-
-      promise.catch(reject);
-    });
-  };
-};
-
-const enchanceInstance = (instance, defaults) => {
-  ['get', 'delete', 'options', 'post', 'put', 'patch'].forEach((method) => {
-    instance[method] = enchanceMethod(instance[method], defaults);
-  });
-};
-
-const create = redaxios.create.bind(redaxios);
-redaxios.create = (config = {}) => {
-  const instance = create(config);
-  enchanceInstance(instance, config);
-  return instance;
-};
-
-export default redaxios;
+export default axios;
