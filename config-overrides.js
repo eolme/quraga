43dc5ccf25037114af41bee9fc654549ec061c91
Anton Petrov <im@petrov.engineer>
2020-05-06 16:59:32 +0400

    perf: optimize css

--- config-overrides.js
+++ config-overrides.js
@@ -3,14 +3,17 @@ const {
   disableEsLint,
   addWebpackPlugin,
   addBabelPlugin,
+  addPostcssPlugins,
   watchAll
 } = require('customize-cra');
 
 const PreloadWebpackPlugin = require('preload-webpack-plugin');
+const CombineMediaQuery = require('postcss-combine-media-query');
 
 module.exports = process.env.NODE_ENV === 'production' ?
   override(
     disableEsLint(),
+    addPostcssPlugins([CombineMediaQuery]),
     addBabelPlugin('@babel/transform-react-constant-elements'),
     addBabelPlugin('@babel/transform-react-inline-elements'),
     addBabelPlugin('transform-react-remove-prop-types'),
