c9bb38183cb0948f3f8001c8fdbfe5f348ec21c4
Anton Petrov <im@petrov.engineer>
2020-05-28 16:20:33 +0400

    fix: revert reset

--- src/hooks/use-panels.js
+++ src/hooks/use-panels.js
@@ -18,7 +18,7 @@ export default function usePanels(initialActivePanel) {
           swipe.enable();
         }
 
-        return nextActivePanel || initialActivePanel;
+        return nextActivePanel;
       });
     });
   }, [setActivePanel]);
