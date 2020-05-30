2fc82263fd6719a5a05d41dd658cd243e6fa6da9
Anton Petrov <im@petrov.engineer>
2020-05-30 20:33:09 +0400

    feat: update game error

--- src/components/GameError.jsx
+++ src/components/GameError.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
 
 import { Group, Header, Cell } from '@vkontakte/vkui';
@@ -8,20 +8,38 @@ import useGlobal from '../hooks/use-global';
 
 const GameError = ({ code }) => {
   const global = useGlobal();
+  const prevent = useRef(false);
+
   let content = null;
   let action = null;
 
   useEffect(() => {
-    const end = () => {
-      global.bus.emit('game:end');
+    const reset = () => {
+      window.requestAnimationFrame(() => {
+        if (!prevent.current) {
+          global.bus.emit('game:end');
+        }
+      });
     };
 
-    global.bus.once('modal:closed', end);
-
+    global.bus.once('modal:closed', reset);
     return () => {
-      global.bus.detach('modal:closed', end);
+      global.bus.detach('modal:closed', reset);
     };
   }, []);
+
+  const startGame = () => {
+    window.requestAnimationFrame(() => {
+      global.bus.emit('game:end');
+      window.requestAnimationFrame(() => {
+        setTimeout(() => {
+          window.requestAnimationFrame(() => {
+            global.bus.emit('game:start');
+          });
+        }, 600);
+      });
+    });
+  };
 
   if (code === 1 || code === 2 || code === 3 || code === 5) {
     let message = null;
@@ -42,18 +60,40 @@ const GameError = ({ code }) => {
     content = `${message} Это вовсе не проблема! Просто создай новую игру или присоединись к другой.`;
 
     const createGame = () => {
+      prevent.current = true;
+      global.store.modal.state = true;
       global.bus.once('modal:closed', () => {
+        if (!global.store.modal.state) {
+          return;
+        }
+        global.store.modal.state = false;
         global.store.mode = 'multi';
         global.store.game = {};
         global.store.join = -1;
-
-        window.requestAnimationFrame(() => {
-          global.bus.emit('game:start');
-        });
+        startGame();
       });
       global.bus.emit('modal:close');
     };
     action = (<Button onClick={createGame} className="Button--blue">Создать новую игру</Button>);
+  } else if (code === 7) {
+    content = 'Тебе одиноко? Это вовсе не проблема! Ты всегда можешь начать одиночную игру.';
+
+    const createGame = () => {
+      prevent.current = true;
+      global.store.modal.state = true;
+      global.bus.once('modal:closed', () => {
+        if (!global.store.modal.state) {
+          return;
+        }
+        global.store.modal.state = false;
+        global.store.mode = 'single';
+        global.store.game = {};
+        global.store.join = -1;
+        startGame();
+      });
+      global.bus.emit('modal:close');
+    };
+    action = (<Button onClick={createGame} className="Button--blue">Начать одиночную игру</Button>);
   } else if (code === 4 || code === 6) {
     content = 'Пользователь еще не зашел в игру';
   }
