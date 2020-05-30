10fc64a9863e4c1de0ea0727d9f13c02af909cf8
Anton Petrov <im@petrov.engineer>
2020-05-30 22:19:48 +0400

    fix: workaround for timers bug

--- src/utils/timers.js
+++ src/utils/timers.js
@@ -1,35 +1,41 @@
+import { platform, ANDROID } from '@vkontakte/vkui';
+import bridge from './bridge';
 import worker from './worker';
 import { load } from 'worker-timers-broker/build/es2019/module';
 import { createLoadWorkerTimers } from 'worker-timers/build/es2019/factories/load-worker-timers';
 
-const safeTimer = (by, fallback) => {
-  return (callback, time) => {
-    if (callback) {
-      try {
-        return by(callback, time);
-      } catch {
-        return fallback(callback, time);
-      }
-    }
-  };
-};
-
-const loadWorkerTimers = createLoadWorkerTimers(load, worker);
-
-const workerSetTimeout = (func, delay) => loadWorkerTimers().setTimeout(func, delay);
-const workerClearTimeout = (timerId) => loadWorkerTimers().clearTimeout(timerId);
-
 window._setTimeout = window.setTimeout.bind(window);
 window._clearTimeout = window.clearTimeout.bind(window);
-
-window.setTimeout = safeTimer(workerSetTimeout, window._setTimeout);
-window.clearTimeout = safeTimer(workerClearTimeout, window._clearTimeout);
-
-const workerSetInterval = (func, delay) => loadWorkerTimers().setInterval(func, delay);
-const workerClearInterval = (timerId) => loadWorkerTimers().clearInterval(timerId);
 
 window._setInterval = window.setInterval.bind(window);
 window._clearInterval = window.setInterval.bind(window);
 
-window.setInterval = safeTimer(workerSetInterval, window._setInterval);
-window.clearInterval = safeTimer(workerClearInterval, window._clearInterval);
+// Web Worker doesn't work well in Android WebView
+const isAndroidWebView = bridge.isWebView() && platform() === ANDROID;
+if (!isAndroidWebView) {
+  const safeTimer = (by, fallback) => {
+    return (callback, time) => {
+      if (callback) {
+        try {
+          return by(callback, time);
+        } catch {
+          return fallback(callback, time);
+        }
+      }
+    };
+  };
+
+  const loadWorkerTimers = createLoadWorkerTimers(load, worker);
+
+  const workerSetTimeout = (func, delay) => loadWorkerTimers().setTimeout(func, delay);
+  const workerClearTimeout = (timerId) => loadWorkerTimers().clearTimeout(timerId);
+
+  window.setTimeout = safeTimer(workerSetTimeout, window._setTimeout);
+  window.clearTimeout = safeTimer(workerClearTimeout, window._clearTimeout);
+
+  const workerSetInterval = (func, delay) => loadWorkerTimers().setInterval(func, delay);
+  const workerClearInterval = (timerId) => loadWorkerTimers().clearInterval(timerId);
+
+  window.setInterval = safeTimer(workerSetInterval, window._setInterval);
+  window.clearInterval = safeTimer(workerClearInterval, window._clearInterval);
+}
